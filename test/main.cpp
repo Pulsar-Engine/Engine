aad1db80a8efa091b19379cf96c4c374326515a6
Élise Valentin <elise.valentin@proton.me>
2025-01-13 15:31:52 +0100

    timeout on the first test

--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
+#include <chrono>
 
 bool simpleWindowVulkan(void)
 {
@@ -17,8 +18,13 @@ bool simpleWindowVulkan(void)
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
+    std::chrono::time_point<std::chrono::high_resolution_clock> lastTime = std::chrono::high_resolution_clock::now();
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        std::chrono::time_point<std::chrono::high_resolution_clock> currentTime = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float, std::chrono::seconds::period> deltaTime = currentTime - lastTime;
+        if (deltaTime.count() > 5.0f)
+            break;
     }
     glfwDestroyWindow(window);
     glfwTerminate();
